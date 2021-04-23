ad78c0f560b51238985c13ab6857ee8af32136e3
Dorime <dorimeer@gmail.com>
2021-04-23 22:27:48 +0300

    added command line argument

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,26 @@
 
 int main(int argc, char* argv[])
 {
+    if (argc != 3)
+    {
+        std::cerr << "wrong number of arguments\nCorrect format is:\n./ray_tracer --source=<path to your *.obj> "
+                     "--output=<path to output image>\n";
+        return -1;
+    }
+
+    // get source and output file
+    std::string source = "";
+    std::string output = "";
+    for (int i = 1; i < argc; i++)
+    {
+        std::string_view arg(argv[i]);
+        if (arg.starts_with("--source")) source = arg.substr(9);
+        if (arg.starts_with("--output")) output = arg.substr(9);
+    }
+    if (source.empty() || output.empty())
+    {
+        std::cerr << "wrong arguments\nSource or output file is missing";
+        return -1;
+    }
     std::cout << "Hello world\n";
 }
